6b26de5d8e99487e676d5c753af4ac71d34151b0
cheer-sea <1723314903@qq.com>
2025-09-07 23:46:16 +0800

    Pytorch 的小实践

---
+++ my_torch_try.py
@@ -0,0 +1,18 @@
+'''
+    这个源文件进行pytorch相关的学习
+'''
+
+import warnings
+# dir 函数，可以查看工具箱的内容
+# dir(pytorch)
+
+# help 函数，可以查看工具箱里的函数怎么使用
+# help(pytorch.tensor)
+help(warnings.catch_warnings)
+
+
+
+
+
+
+
